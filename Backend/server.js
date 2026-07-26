eca33cfa309f23d7b10b4114221aff37ddb11cf0
Abanoub Kerols <abanoubkerols@gmail.com>
2026-07-26 18:46:20 +0300

    feat: initialize Express project, configure CORS, setup MongoDB, add models, password hashing, and task indexes

---
+++ Backend/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./src/config/db');
+
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
